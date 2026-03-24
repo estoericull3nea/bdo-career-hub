efc4a58d290924effd30499d77400bb1d0462d32
Ericson Palisoc <palisocericson87@gmail.com>
2026-03-24 22:16:36 +0800

    fix: improve uninstall process in uninstall.php by adding braces for exit condition and adding comments for clarity

--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,10 @@
 <?php
-if (!defined('WP_UNINSTALL_PLUGIN')) exit;
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
+}
+
 global $wpdb;
+// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery,WordPress.DB.DirectDatabaseQuery.NoCaching,WordPress.DB.DirectDatabaseQuery.SchemaChange -- Uninstall routine must remove plugin-created table.
 $wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}job_applications");
+
 delete_option('jpm_settings');
